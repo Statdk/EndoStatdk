5ee2f887b01c28b3b6e5ba0d4348a3e2845681ce
Statdk <morgirme@gmail.com>
2021-03-05 11:36:43 -0800

    Organized commands into classes

--- bot/EndoStatdk.py
+++ bot/EndoStatdk.py
@@ -25,12 +25,11 @@ client = commands.Bot(command_prefix='.')
 logChannel = 722277448934359073
 vol = 20
 
-# Methods/Functions______________________________________________________________
-
-# outputs True if 'f' is a number
+# region Methods/Functions
 
 
 def testNumber(arg):
+    """outputs True if 'f' is a number"""
     try:
         x = float(arg)
     except ValueError:
@@ -41,7 +40,9 @@ def testNumber(arg):
 async def log(arg):
     await client.get_channel(logChannel).send(f'Error:\n{arg}\n{datetime.now()}\n...')
 
-# Events_________________________________________________________________________
+# endregion
+
+# region Events
 
 
 @client.event
@@ -51,157 +52,163 @@ async def on_ready():
     await client.get_channel(logChannel).send(f'Logged in as\n{client.user.name}\n{client.user.id}\n{datetime.now()}\n...')
     await client.change_presence(activity=discord.Activity(name="", type=4))
 
-# Commands_______________________________________________________________________
+# endregion
+
+# region Commands
 
 
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')
+class Fun():
+    @client.command(aliases=['8ball'])
+    async def _8ball(ctx, *, question=None):
+        """Ask the 8ball a question"""
+        if question == None:
+            await ctx.send(f"Command syntax: `.8ball [question]`")
+        elif testNumber(question):
+            await ctx.send(f"You imbecil! {question} is a number!")
+        else:
+            responses = ["It is certain.",
+                         "It is decidedly so.",
+                         "Without a doubt.",
+                         "Yes - definitely.",
+                         "You may rely on it.",
+                         "As I see it, yes.",
+                         "Most likely.",
+                         "Outlook good.",
+                         "Yes.",
+                         "Signs point to yes.",
+                         "Reply hazy, try again.",
+                         "Ask again later.",
+                         "Better not tell you now.",
+                         "Cannot predict now.",
+                         "Concentrate and ask again.",
+                         "Don't count on it.",
+                         "My reply is no.",
+                         "My sources say no.",
+                         "Outlook not so good.",
+                         "Very doubtful."]
+            await ctx.send(f"{random.choice(responses)}")
 
+    @client.command()
+    async def status(ctx, text=None, type=1):
+        if text == None:
+            await client.change_presence(activity=discord.Activity(name="", type=4))
+            return
+        await client.change_presence(activity=discord.Activity(name=text, type=type))
 
-@client.command(aliases=['8ball'])
-async def _8ball(ctx, *, question=None):
-    """Ask the 8ball a question"""
-    if question == None:
-        await ctx.send(f"Command syntax: `.8ball [question]`")
-    elif testNumber(question):
-        await ctx.send(f"You imbecil! {question} is a number!")
-    else:
-        responses = ["It is certain.",
-                     "It is decidedly so.",
-                     "Without a doubt.",
-                     "Yes - definitely.",
-                     "You may rely on it.",
-                     "As I see it, yes.",
-                     "Most likely.",
-                     "Outlook good.",
-                     "Yes.",
-                     "Signs point to yes.",
-                     "Reply hazy, try again.",
-                     "Ask again later.",
-                     "Better not tell you now.",
-                     "Cannot predict now.",
-                     "Concentrate and ask again.",
-                     "Don't count on it.",
-                     "My reply is no.",
-                     "My sources say no.",
-                     "Outlook not so good.",
-                     "Very doubtful."]
-        await ctx.send(f"{random.choice(responses)}")
+    @client.command(aliases=['random'])
+    async def rand(ctx, *, args="0 100 1"):
+        """Returns an amount of random numbers"""
+        # Arguments are: Minimum, Maximum, and Amount
+        args = args.split()
 
-
-@client.command(aliases=['purge', 'prune', 'delete', 'del'])
-@commands.has_permissions(manage_messages=True)
-async def clear(ctx, *, amount=""):
-    """Clears a number of messages"""
-    if amount == "":
-        await ctx.channel.purge(limit=1)
-        await ctx.send("Command syntax: `.delete <number of messages>`", delete_after=3)
-    elif not testNumber(amount):
-        await ctx.channel.purge(limit=1)
-        await ctx.send("That's not a number...", delete_after=3)
-    else:
-        await ctx.channel.purge(limit=int(amount) + 1)
-        log(f"Deleted {amount} messages in {ctx.get_channel}")
-
-
-@client.command(aliases=['random'])
-async def rand(ctx, *, args="0 100 1"):
-    # Arguments are: Minimum, Maximum, and Amount
-    args = args.split()
-
-    tosend = ""
-    try:
-        for i in range(int(args[2])):
-            tosend += f"{random.choice(range(int(args[0]), int(args[1])))}\n"
-    except Exception as e:
-        await log(e)
-        await ctx.send("Command syntax: '.random <minimum> <maximum> <number of messages>'")
-        return
-
-    await ctx.send(tosend)
-
-
-@client.command(aliases=['addition', 'ad'])
-async def add(ctx, a, b):
-    if testNumber(a) and testNumber(b):
-        await ctx.send(float(a) + float(b))
-    else:
-        await ctx.send('Command syntax:: `.add [first] [second]`')
-
-
-@client.command(aliases=['subtraction', 'sub'])
-async def subtract(ctx, a, b):
-    if testNumber(a) and testNumber(b):
-        await ctx.send(float(a) - float(b))
-    else:
-        await ctx.send('Command syntax:: `.subtract [first] [second]`')
-
-
-@client.command(aliases=['multiplication', 'mult'])
-async def multiply(ctx, a, b):
-    if testNumber(a) and testNumber(b):
-        await ctx.send(float(a) * float(b))
-    else:
-        await ctx.send('Command syntax:: `.multiply [first] [second]`')
-
-
-@client.command(aliases=['division', 'div'])
-async def divide(ctx, a, b):
-    if testNumber(a) and testNumber(b):
-        await ctx.send(float(a) / float(b))
-    else:
-        await ctx.send('Command syntax:: `.divide [first] [second]`')
-
-
-@client.command()
-async def math(ctx, *, arg):
-    """Performs simple math operations from left to right"""
-    arg = arg.split()
-    ans = float(arg[0])
-
-    args = 0
-    f = 0
-    for i in arg:
-        f += 1
-
+        tosend = ""
         try:
-            if i == "+":
-                ans += float(arg[f])
-                args += 1
-            elif i == "-":
-                ans -= float(arg[f])
-                args += 1
-            elif i == "*":
-                ans *= float(arg[f])
-                args += 1
-            elif i == "/":
-                ans /= float(arg[f])
-                args += 1
+            for i in range(int(args[2])):
+                tosend += f"{random.choice(range(int(args[0]), int(args[1])))}\n"
         except Exception as e:
-            await ctx.send(e)
-            await ctx.send("Command syntax:: `.math a operator b operator c ...`")
+            await log(e)
+            await ctx.send("Command syntax: '.random <minimum> <maximum> <number of messages>'")
             return
 
-    await ctx.send(f"Result: {ans}")
+        await ctx.send(tosend)
 
 
-@client.command()
-@commands.has_permissions(manage_messages=True)
-async def shutdown(ctx):
-    await ctx.send(f"Shutting down\n{datetime.now()}")
-    exit()
+class Moderation():
+    @client.command(aliases=['purge', 'prune', 'delete', 'del'])
+    @commands.has_permissions(manage_messages=True)
+    async def clear(ctx, *, amount=""):
+        """Clears a number of messages"""
+        if amount == "":
+            await ctx.channel.purge(limit=1)
+            await ctx.send("Command syntax: `.delete <number of messages>`", delete_after=3)
+        elif not testNumber(amount):
+            await ctx.channel.purge(limit=1)
+            await ctx.send("That's not a number...", delete_after=3)
+        else:
+            await ctx.channel.purge(limit=int(amount) + 1)
+            log(f"Deleted {amount} messages in {ctx.get_channel}")
 
 
-@client.command()
-async def status(ctx, text=None, type=1):
-    if text == None:
-        await client.change_presence(activity=discord.Activity(name="", type=4))
-        return
-    await client.change_presence(activity=discord.Activity(name=text, type=type))
+class Math():
+    @client.command(aliases=['addition', 'ad'])
+    async def add(self, ctx, a, b):
+        """Adds two numbers"""
+        if testNumber(a) and testNumber(b):
+            await ctx.send(float(a) + float(b))
+        else:
+            await ctx.send('Command syntax:: `.add [first] [second]`')
+
+    @client.command(aliases=['subtraction', 'sub'])
+    async def subtract(self, ctx, a, b):
+        """Subtracts two numbers"""
+        if testNumber(a) and testNumber(b):
+            await ctx.send(float(a) - float(b))
+        else:
+            await ctx.send('Command syntax:: `.subtract [first] [second]`')
+
+    @client.command(aliases=['multiplication', 'mult'])
+    async def multiply(self, ctx, a, b):
+        """Multiplies two numbers"""
+        if testNumber(a) and testNumber(b):
+            await ctx.send(float(a) * float(b))
+        else:
+            await ctx.send('Command syntax:: `.multiply [first] [second]`')
+
+    @client.command(aliases=['division', 'div'])
+    async def divide(self, ctx, a, b):
+        """Divides two numbers"""
+        if testNumber(a) and testNumber(b):
+            await ctx.send(float(a) / float(b))
+        else:
+            await ctx.send('Command syntax:: `.divide [first] [second]`')
+
+    @client.command()
+    async def math(self, ctx, *, arg):
+        """Performs simple math operations from left to right"""
+        arg = arg.split()
+        ans = float(arg[0])
+
+        args = 0
+        f = 0
+        for i in arg:
+            f += 1
+
+            try:
+                if i == "+":
+                    ans += float(arg[f])
+                    args += 1
+                elif i == "-":
+                    ans -= float(arg[f])
+                    args += 1
+                elif i == "*":
+                    ans *= float(arg[f])
+                    args += 1
+                elif i == "/":
+                    ans /= float(arg[f])
+                    args += 1
+            except Exception as e:
+                await ctx.send(e)
+                await ctx.send("Command syntax:: `.math a operator b operator c ...`")
+                return
+
+        await ctx.send(f"Result: {ans}")
 
 
-# MUSIC_________________________________________________________________________________
+class Misc():
+    @client.command()
+    async def ping(ctx):
+        await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')
+
+    @client.command()
+    @commands.has_permissions(manage_messages=True)
+    async def shutdown(ctx):
+        await ctx.send(f"Shutting down\n{datetime.now()}")
+        exit()
+
+# endregion
+
+# region MUSIC
+
 
 # Suppress noise about console usage from errors
 youtube_dl.utils.bug_reports_message = lambda: ''
@@ -344,6 +351,8 @@ class Music(commands.Cog):
                     "Author not connected to a voice channel.")
         elif ctx.voice_client.is_playing():
             ctx.voice_client.stop()
+
+# endregion
 
 
 client.add_cog(Music(client))
