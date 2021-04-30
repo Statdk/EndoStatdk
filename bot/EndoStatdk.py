e263303acdc1eca95bd75749574584699165799d
Statdk <morgirme@gmail.com>
2021-04-30 14:48:06 -0700

    Fancified discord messages

--- bot/EndoStatdk.py
+++ bot/EndoStatdk.py
@@ -9,7 +9,7 @@ from os import path
 import discord
 import youtube_dl
 from discord import player
-from discord.ext import commands
+from discord.ext import tasks, commands
 
 if path.exists('./bot/config.json'):
     with open('./bot/config.json') as f:
@@ -23,7 +23,6 @@ else:
 
 client = commands.Bot(command_prefix='.')
 logChannel = 722277448934359073
-vol = 20
 
 # region Methods/Functions
 
@@ -38,7 +37,21 @@ def testNumber(arg):
 
 
 async def log(arg):
-    await client.get_channel(logChannel).send(f'Error:\n{arg}\n{datetime.now()}\n...')
+    await client.get_channel(logChannel).send(formatCode(f'Error:\n{arg}\n{datetime.now()}\n...'), "diff")
+
+
+def formatCode(arg, type=""):
+    """Converts formatting to one suited for discord
+
+    "diff" = red;
+    "css" = red-orange;
+    "fix" = yellow;
+    "ini" with [] = blue;
+    "json" with "" = green;
+    " " = none
+    """
+    toSend = f"```{type}\n{arg}\n```"
+    return toSend
 
 # endregion
 
@@ -49,6 +62,7 @@ async def log(arg):
 async def on_ready():
     print(
         f"Logged in as\n{client.user.name}\n{client.user.id}\n{datetime.now()}\n...")
+
     await client.get_channel(logChannel).send(f'Logged in as\n{client.user.name}\n{client.user.id}\n{datetime.now()}\n...')
     await client.change_presence(activity=discord.Activity(name="", type=4))
 
@@ -114,16 +128,16 @@ class Fun():
 
 
 class Moderation():
-    @client.command(aliases=['purge', 'prune', 'delete', 'del'])
+    @client.command(aliases=['purge', 'prune', 'del'])
     @commands.has_permissions(manage_messages=True)
-    async def clear(self, *, amount=""):
+    async def delete(self, *, amount=""):
         """Clears a number of messages"""
         if amount == "":
             await self.channel.purge(limit=1)
-            await self.send("Command syntax: `.delete <number of messages>`", delete_after=3)
+            await self.send(formatCode("Command syntax: `.delete <number of messages>`", "diff"), delete_after=3)
         elif not testNumber(amount):
             await self.channel.purge(limit=1)
-            await self.send("That's not a number...", delete_after=3)
+            await self.send(formatCode("That's not a number...", "diff"), delete_after=3)
         else:
             await self.channel.purge(limit=int(amount) + 1)
             log(f"Deleted {amount} messages in {self.get_channel}")
@@ -134,45 +148,43 @@ class Math():
     async def add(self, a, b):
         """Adds two numbers"""
         if testNumber(a) and testNumber(b):
-            await self.send(float(a) + float(b))
+            await self.send(formatCode(f"[{float(a) + float(b)}]", "ini"))
         else:
-            await self.send('Command syntax:: `.add [first] [second]`')
+            await self.send(formatCode('Command syntax:: `.add [first] [second]`', "diff"))
 
     @client.command(aliases=['subtraction', 'sub'])
     async def subtract(self, a, b):
         """Subtracts two numbers"""
         if testNumber(a) and testNumber(b):
-            await self.send(float(a) - float(b))
+            await self.send(formatCode(f"[{float(a) - float(b)}]", "ini"))
         else:
-            await self.send('Command syntax:: `.subtract [first] [second]`')
+            await self.send(formatCode('Command syntax:: `.subtract [first] [second]`', "diff"))
 
     @client.command(aliases=['multiplication', 'mult'])
     async def multiply(self, a, b):
         """Multiplies two numbers"""
         if testNumber(a) and testNumber(b):
-            await self.send(float(a) * float(b))
+            await self.send(formatCode(f"[{float(a) * float(b)}]", "ini"))
         else:
-            await self.send('Command syntax:: `.multiply [first] [second]`')
+            await self.send(formatCode('Command syntax:: `.multiply [first] [second]`', "diff"))
 
     @client.command(aliases=['division', 'div'])
     async def divide(self, a, b):
         """Divides two numbers"""
         if testNumber(a) and testNumber(b):
-            await self.send(float(a) / float(b))
+            await self.send(formatCode(f"[{float(a) / float(b)}]", "ini"))
         else:
-            await self.send('Command syntax:: `.divide [first] [second]`')
+            await self.send(formatCode('Command syntax:: `.divide [first] [second]`', "diff"))
 
-    @client.command()
+    @ client.command(aliases=['compute'])
     async def math(self, *, arg):
         """Performs simple math operations from left to right"""
         arg = arg.split()
         ans = float(arg[0])
-
         args = 0
-        f = 0
-        for i in arg:
-            f += 1
+        f = 1
 
+        for i in arg:
             try:
                 if i == "+":
                     ans += float(arg[f])
@@ -186,27 +198,30 @@ class Math():
                 elif i == "/":
                     ans /= float(arg[f])
                     args += 1
+                f += 1
+
             except Exception as e:
                 await self.send(e)
-                await self.send("Command syntax:: `.math a operator b operator c ...`")
+                await self.send(formatCode("Command syntax:: `.math [a] [operator] [b] [operator] [c] ...`", "diff"))
                 return
 
-        await self.send(f"Result: {ans}")
+        await self.send(formatCode(f"Result: [{ans}] with [{args}] arguments", "ini"))
 
 
 class Management():
-    @client.command()
+    @ client.command()
     async def ping(self):
-        await self.send(f'Pong! `{round(client.latency * 1000)}ms`')
+        await self.send(formatCode(f'Pong! [{round(client.latency * 1000)}ms]', "ini"))
 
-    @client.command()
-    @commands.has_permissions(manage_messages=True)
+    @ client.command()
+    @ commands.has_permissions(manage_messages=True)
     async def shutdown(self):
-        await self.send(f"Shutting down\n{datetime.now()}")
+        await self.send(formatCode(f"Shutting down\n{datetime.now()}\n...", "diff"))
+        log("Shutting down")
         client.logout()
         exit()
 
-    @client.command()
+    @ client.command()
     async def say(self, channel_id, message):
         try:
             if (client.is_owner(self.author)):
@@ -274,45 +289,25 @@ class YTDLSource(discord.PCMVolumeTransformer):
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.queueList = []
+        self.vol = 20
 
-    @commands.command()
+    @ commands.command()
     async def join(self, ctx, *, channel: discord.VoiceChannel):
         """Joins a voice channel"""
 
         if ctx.voice_client is not None:
             return await ctx.voice_client.move_to(channel)
 
-        await channel.connect()
+        if channel is not None:
+            await channel.connect()
+        elif ctx.author.voice:
+            await ctx.author.voice.channel.connect()
+        else:
+            await ctx.send(formatCode("Could not connect to voice channel", "diff"))
 
-    @commands.command()
-    async def play(self, ctx, *, query):
-        """Plays a file from the local filesystem"""
-
-        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
-        ctx.voice_client.play(source, after=lambda e: print(
-            'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(query))
-
-        ctx.voice_client.source.volume = vol / 100
-        await client.change_presence(activity=discord.Activity(name=player.title, type=2))
-
-    @commands.command()
-    async def yt(self, ctx, *, url):
-        """Plays from a url (almost anything youtube_dl supports)"""
-
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            ctx.voice_client.play(player, after=lambda e: print(
-                'Player error: %s' % e) if e else None)
-
-        await ctx.send('Now playing: {}'.format(player.title))
-
-        ctx.voice_client.source.volume = vol / 100
-        await client.change_presence(activity=discord.Activity(name=player.title, type=2))
-
-    @commands.command()
-    async def stream(self, ctx, *, url):
+    @ commands.command()
+    async def play(self, ctx, *, url):
         """Streams from a url (same as yt, but doesn't predownload)"""
 
         async with ctx.typing():
@@ -320,40 +315,94 @@ class Music(commands.Cog):
             ctx.voice_client.play(player, after=lambda e: print(
                 'Player error: %s' % e) if e else None)
 
-        await ctx.send('Now playing: {}'.format(player.title))
+        await ctx.send(formatCode('Now playing: "{}"'.format(player.title), json))
 
-        ctx.voice_client.source.volume = vol / 100
+        ctx.voice_client.source.volume = self.vol / 100
         await client.change_presence(activity=discord.Activity(name=player.title, type=2))
 
-    @commands.command(aliases=['vol', 'v'])
+    @ commands.command(aliases=['vol', 'v'])
     async def volume(self, ctx, volume: int):
         """Changes the player's volume"""
-        vol = volume
+        self.vol = volume
 
         if ctx.voice_client is None:
-            return await ctx.send("Not connected to a voice channel.")
+            return await ctx.send(formatCode("Not connected to a voice channel.", "diff"))
 
         ctx.voice_client.source.volume = volume / 100
-        await ctx.send("Changed volume to {}%".format(volume))
+        await ctx.send(formatCode("Changed volume to [{}%]".format(volume), "ini"))
 
-    @commands.command()
-    async def pause(self, ctx):
-        """Pauses and unpauses the player"""
-        if client.is_paused():
-            client.resume()
+    @ commands.command(aliases=['q'])
+    async def queue(self, ctx, *, url):
+        """Shows the current queue or adds songs"""
+        toSend = ""
+
+        try:
+            self.queueList.append(await YTDLSource.from_url(url, loop=self.bot.loop, stream=True))
+            await ctx.send(formatCode(f'Added "{self.queueList[-1]}"', json))
+
+        except Exception as e:
+            log(e)
+
+        if not self.queueList == []:
+            toSend += "Current Queue:"
+            for item in self.queueList:
+                toSend += f'\n> {self.queueList.index(item)}. "{item.title}"'
         else:
-            client.pause()
+            await ctx.send(formatCode("Queue is empty", "fix"))
 
-    @commands.command()
+        if not toSend == "":
+            await ctx.send(formatCode(toSend, "json"))
+
+    @ tasks.loop(seconds=5)
+    async def queuer(self, ctx):
+        try:
+            if self.queueList == [] or not self.player.is_connected():
+                self.queuer.stop()
+            if not self.player.is_playing():
+                player = self.queueList[0]
+                self.queueList.pop(0)
+
+                ctx.voice_client.play(player, after=lambda e: print(
+                    'Player error: %s' % e) if e else None)
+
+                await ctx.send(formatCode('Now playing: "{}"'.format(player.title), json))
+        except Exception as e:
+            log(e)
+
+    @ commands.command(aliases=['cl', 'cls'])
+    async def clear(self, ctx):
+        """Clears the queue"""
+        self.queueList.clear()
+        await ctx.send(formatCode("Queue Cleared", "fix"))
+
+    @ commands.command()
+    async def pause(self, ctx):
+        """Pauses/Unpauses the bot"""
+        try:
+            if player.is_paused():
+                player.resume()
+            elif player.is_playing():
+                player.pause()
+            else:
+                await ctx.send(formatCode("Play something first", "fix"))
+        except Exception:
+            await ctx.send(formatCode("Player is inactive", "fix"))
+
+    @ commands.command()
     async def stop(self, ctx):
         """Stops and disconnects the bot from voice"""
+
+        self.queueList.clear()
 
         await client.change_presence(activity=discord.Activity(name="", type=4))
         await ctx.voice_client.disconnect()
 
-    @play.before_invoke
-    @yt.before_invoke
-    @stream.before_invoke
+    @ play.after_invoke
+    async def ensure_queuer(self, ctx):
+        if not self.queuer.is_running():
+            self.queuer.start()
+
+    @ play.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
